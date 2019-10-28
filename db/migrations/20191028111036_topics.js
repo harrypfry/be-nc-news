de385361faafd43daa4a48d0ee6de9b27ad88e4d
Harry Fry <harrypfry@gmail.com>
2019-10-28 17:03:20 +0000

    Migrations added

---
+++ db/migrations/20191028111036_topics.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("topics", topicsTable => {
+    console.log("creating table 'topics'...");
+    topicsTable
+      .string("slug")
+      .primary()
+      .notNullable();
+    topicsTable.string("description").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  console.log("dropping table 'topics'...");
+  return knex.schema.dropTable("topics");
+};
---
+++ db/migrations/20191028112433_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  console.log("creating table 'users'...");
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .primary()
+      .unique();
+    usersTable.string("avatar_url").notNullable();
+    usersTable.string("name").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  console.log("dropping table 'users'...");
+  return knex.schema.dropTable("users");
+};
