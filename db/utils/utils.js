bfc3365480070ef7a9a04577c9de332d1d782822
Harry Fry <harrypfry@gmail.com>
2019-10-28 14:46:53 +0000

    Format date util complete

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,12 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  const newArr = [];
+  list.forEach(item => {
+    let { created_at, ...rest } = item;
+    created_at = new Date(created_at);
+    newArr.push({ ...rest, created_at });
+  });
+  return newArr;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,112 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("returns empty array when empty array is passed", () => {
+    const inputArr = [];
+    const actualResult = formatDates(inputArr);
+    const expectedResult = [];
+    expect(actualResult).to.deep.equal(expectedResult);
+  });
+  it("Doesn't mutate the original array", () => {
+    const inputArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const inputArrCopy = [...inputArr];
+    formatDates(inputArr);
+    expect(inputArr).to.deep.equal(inputArrCopy);
+  });
+  it("returns correctly formatted array length one when array of length one is passed", () => {
+    const inputArr = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+    const actualResult = formatDates(inputArr);
+    const expectedResult = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: new Date(inputArr[0].created_at),
+        votes: 100
+      }
+    ];
+    expect(actualResult).to.deep.equal(expectedResult);
+  });
+  it("returns correctly formatted array length greater than one when array of length greater than one is passed", () => {
+    const inputArr = [
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: 1416140514171
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: 1289996514171
+      },
+      {
+        title: "Student SUES Mitch!",
+        topic: "mitch",
+        author: "rogersop",
+        body:
+          "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+        created_at: 1163852514171
+      }
+    ];
+    const actualResult = formatDates(inputArr);
+    const expectedResult = [
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: new Date(1416140514171)
+      },
+      {
+        title: "Eight pug gifs that remind me of mitch",
+        topic: "mitch",
+        author: "icellusedkars",
+        body: "some gifs",
+        created_at: new Date(1289996514171)
+      },
+      {
+        title: "Student SUES Mitch!",
+        topic: "mitch",
+        author: "rogersop",
+        body:
+          "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+        created_at: new Date(1163852514171)
+      }
+    ];
+    expect(actualResult).to.deep.equal(expectedResult);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
