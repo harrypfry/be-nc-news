2fbea841dafb29391334f6f66dfc568069931549
Harry Fry <harrypfry@gmail.com>
2019-11-01 13:02:43 +0000

    Listen.js amended

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const { PORT = 9090 } = process.env;
 
-applicationCache.addEventListener(PORT, () => {
+const app = require("./app");
+
+app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
