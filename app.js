f031b591ecacf96eaf2f2391c3150a660861c904
Harry Fry <harrypfry@gmail.com>
2019-11-19 15:36:56 +0000

    Corrected CORS

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const cors = require("cors");
 
 const express = require("express");
-app.use(cors());
 const app = express();
 app.use(express.json());
 
 const apiRouter = require("./routes/api-router");
-app.use(cors());
 
 const { customErrors, psqlErrors } = require("./errors/error");
 app.use(cors());
